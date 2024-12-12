ae58cbe7155c713cec47dcdbde0ca6c69e723c0e
Parth Vishwakarma <parthv1995@gmail.com>
2024-12-12 16:11:06 +0530

    fix(env): oops, correction

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
+require('dotenv').config()
 const express = require("express");
 var { graphqlHTTP } = require("express-graphql");
 const { rootSchema } = require("./schemas/rootSchema");
 // var { createHandler } = require("graphql-http/lib/use/express")
 const mongoose = require("mongoose");
 
-const {dbUser} = require("./server/users")
-
 const app = express();
 
-mongoose.connect(`mongodb+srv://${dbUser.MONGO_USER}:${dbUser.MONGO_PASSWORD}@cluster0.iffee.mongodb.net/${dbUser.DB_APPDATA}?retryWrites=true&w=majority&appName=Cluster0`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.iffee.mongodb.net/${process.env.DB_APPDATA}?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=> console.log('connection succeeded'))
 .catch(err => {
   console.log('connection failed')
